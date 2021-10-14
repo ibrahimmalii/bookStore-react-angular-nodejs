4d9b433f54a79363bf6df552aab0a6029419549e
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2021-10-14 22:03:42 +0200

    setub bootstrap and fontawesome and axios with router

--- www/BookStore/src/App.js
+++ www/BookStore/src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
 
 function App() {
   return (
