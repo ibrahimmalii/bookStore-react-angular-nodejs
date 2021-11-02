e25fbd0036f5197f0484a990e664fd6ac0f5aa5c
George Samy <george.samy2011@gmail.com>
2021-11-02 20:05:27 +0200

    navbar added

--- www/book-store/src/app/components/shared/header/header.component.ts
+++ www/book-store/src/app/components/shared/header/header.component.ts
@@ -1,3 +1,4 @@
+import { ApiService } from './../../../services/api.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
+  books: any;
+  responseGet: Boolean = false
   ngOnInit(): void {
   }
 
+  getCatBooks(id:string){
+
+    const token = localStorage.token
+    this.apiService.get(`http://localhost:8080/api/categories/${id}`,
+      { headers: { 'Authorization': `Bearer ${token}` } })
+      .subscribe(res => {
+        console.log(res)
+        this.books = res;
+        this.responseGet = true
+      },
+      (err => {
+        console.log(err);
+      }))
+  }
 }
