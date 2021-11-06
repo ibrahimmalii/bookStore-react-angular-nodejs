87e0bd5e9c5dc28e6775c353c534470d78a47771
MohamedHossameldenhabishie <mohammedhossamelden10@gmail.com>
2021-11-06 13:00:21 +0200

    add home and sidebar

--- www/book-store/src/app/components/main-page/main-page.module.ts
+++ www/book-store/src/app/components/main-page/main-page.module.ts
@@ -1,8 +1,15 @@
+// import { MaxLengthPipe } from './../shared/pipes/max-length.pipe';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+// import { MaxLengthPipe } from '../shared/pipes/max-length.pipe';
+// import {NgPipesModule} from 'ngx-pipes';
+import {MatIconModule} from '@angular/material/icon';
+// import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
+// import {NgPipesModule} from 'ngx-pipes';
+
 
 const routes: Routes = [
   {path : '', component : HomeComponent}
@@ -10,10 +17,10 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
-    CommonModule, SharedModule, RouterModule.forChild(routes)
+    CommonModule, SharedModule, RouterModule.forChild(routes),MatIconModule,
   ]
 })
 export class MainPageModule { }
--- www/book-store/src/app/components/shared/shared.module.ts
+++ www/book-store/src/app/components/shared/shared.module.ts
@@ -4,6 +4,10 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
+// import { MaxLengthPipe } from './pipes/max-length.pipe';
+// import {NgPipesModule} from 'ngx-pipes';
+
 
 
 
@@ -12,10 +16,10 @@ import { BookDetailsComponent } from './book-details/book-details.component';
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
-    BookDetailsComponent
+    BookDetailsComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,NgxSliderModule
   ],
   exports :[
     HeaderComponent, FooterComponent, SidebarComponent, BookDetailsComponent
