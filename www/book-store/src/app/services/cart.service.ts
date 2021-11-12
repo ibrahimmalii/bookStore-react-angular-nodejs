ba8349f6321c1a78f380366c2ee20a2558d256cd
ibrahim ali <eng.ibrahimmalii@gmail.com>
2021-11-12 15:20:39 +0200

    update cart page with all functionality

--- www/book-store/src/app/services/cart.service.ts
+++ www/book-store/src/app/services/cart.service.ts
@@ -7,51 +7,55 @@ import { UserService } from './user.service';
 })
 export class CartService {
 
-  constructor(private userService:UserService, private router : Router) { }
+  constructor(private userService: UserService, private router: Router) { }
 
-  toCart(book:any){
-     let itemCart=[];
-    if(!this.userService.isLogged()){
+  toCart(book: any) {
+    let itemCart = [];
+    if (!this.userService.isLogged()) {
       this.router.navigateByUrl('/auth/login')
     }
-    else{
-    let carDataNull =localStorage.getItem('localCart');
-    if(carDataNull == null){
-      let storeDataGet :any =[];
-      storeDataGet.push(book);
-      localStorage.setItem("localCart",JSON.stringify(storeDataGet));
-    }
-    else{
-      var id = book._id;
-      let index :number= -1;
-      itemCart =JSON.parse((localStorage.getItem("localCart"))!)  ;
-      for(let i=0; i<itemCart.length;i++){
-        if(parseInt(id) ===parseInt(itemCart[i]._id)){
-         itemCart[i].amount =book.amount;
-         index =i;
-         break;
+    else {
+      let carDataNull = localStorage.getItem('localCart');
+      if (carDataNull == null) {
+        let storeDataGet: any = [];
+        book.quantity = 1;
+        book.numberOfSells = book.quantity * book.price
+        storeDataGet.push(book);
+        localStorage.setItem("localCart", JSON.stringify(storeDataGet));
+      }
+      else {
+        var id = book._id;
+        let index: number = -1;
+        itemCart = JSON.parse((localStorage.getItem("localCart"))!);
+        for (let i = 0; i < itemCart.length; i++) {
+          if (parseInt(id) === parseInt(itemCart[i]._id)) {
+            itemCart[i].amount = book.amount;
+            index = i;
+            break;
+          }
+        }
+        if (index == -1) {
+          book.quantity = 1;
+          book.numberOfSells = book.quantity * book.price
+          itemCart.push(book);
+          localStorage.setItem("localCart", JSON.stringify(itemCart))
+        } else {
+          localStorage.setItem("localCart", JSON.stringify(itemCart))
         }
       }
-      if(index==-1){
-        itemCart.push(book);
-        localStorage.setItem("localCart",JSON.stringify(itemCart))
-      }else{
-        localStorage.setItem("localCart",JSON.stringify(itemCart))
-      }
-    }
-    this.cartNumberFunc();
-    // localStorage.setItem("localCart",JSON.stringify(book))
+      this.cartNumberFunc();
+      // localStorage.setItem("localCart",JSON.stringify(book))
     }
   }
 
 
 
-cartNumberFunc(){
-  var cartValue =JSON.parse( localStorage.getItem('localCart')!);
+  cartNumberFunc() {
+    var cartValue = JSON.parse(localStorage.getItem('localCart')!);
 
- this.userService.cartSubject.next(cartValue.length)
-//  console.log(this.cartNumber);
+    this.userService.cartSubject.next(cartValue.length)
+    //  console.log(this.cartNumber);
+  }
 }
-  }
 
 
