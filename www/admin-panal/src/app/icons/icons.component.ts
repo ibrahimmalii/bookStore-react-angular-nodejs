47def66f6cbed4ff7414952a242a9014c3ea0358
ibrahim ali <eng.ibrahimmalii@gmail.com>
2021-11-16 15:02:48 +0200

    update and add product

--- www/admin-panal/src/app/icons/icons.component.ts
+++ www/admin-panal/src/app/icons/icons.component.ts
@@ -16,14 +16,20 @@ export class IconsComponent implements OnInit {
   pageOfItems: Array<any>;
   isPageLoaded: boolean = false;
   bookUpdated:boolean = false;
+  categories:any;
 
   form : FormGroup = new FormGroup({})
   ngOnInit() {
 
+
     this.http.get('http://localhost:8080/api/books').subscribe(response => {
       this.books = response;
       this.items = Array(150).fill(0).map((x, i) => ({ id: (i + 1), name: `Item ${i + 1}` }));
-      this.isPageLoaded = true
+      this.http.get('http://localhost:8080/api/categories').subscribe(response=>{
+        this.categories = response
+        console.log(response)
+        this.isPageLoaded = true
+      })
     })
   }
 
@@ -50,8 +56,8 @@ export class IconsComponent implements OnInit {
     }
   }
 
-  addNew(rate:any, title:string, author:string, amount:any, price:any, description:string, avatar:any){
-    this.http.post('http://localhost:8080/api/books', {rate, title, author, amount, price, description, avatar}).subscribe(response=>{
+  addNew(rate:any, title:string, author:string, amount:any, price:any, description:string, avatar:any, category:any){
+    this.http.post('http://localhost:8080/api/books', {rate, title, author, amount, price, description, avatar, category}).subscribe(response=>{
       console.log(response)
       location.reload()
     })
